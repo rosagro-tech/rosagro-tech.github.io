7187f2b4c91c71fffcfa673c167c9da764e7ffdf
Egor Rumyantsev <RumyantsevGT@ya.ru>
2020-04-10 15:47:28 +0300

    Add files via upload

---
+++ shop/script.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+  anchors: ['page1', 'page2', 'page3', 'page4'],
+  navigationTooltips: ['Магазин', 'Наука', 'Дача', 'Grow'],
+  css3: true,
+  scrollingSpeed: 1000,
+  navigation: true,
+  slidesNavigation: true,
+  responsiveHeight: 330,
+  dragAndMove: true,
+  dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
+  controlArrows: false
+});
